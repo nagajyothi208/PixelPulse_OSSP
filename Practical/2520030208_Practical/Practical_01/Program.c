7f7f84cfe9a8a4f3e86956dd1d2d4da65a6366cb
nagajyothi208 <arikatlanagajyothi347@gmail.com>
2026-09-12 10:04:04 +0530

    Update Program.c

---
+++ Practical/2520030208_Practical/Practical_01/Program.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+int main()
+{
+int pid;
+pid=fork();
+if(pid==0)
+{
+printf("Child PID=%d\n",getpid());
+execl("/bin/ls","ls",NULL);
+}
+else
+{
+printf("Parent PID=%d\n",getpid());
+wait(NULL);
+}
+return 0;
+}
